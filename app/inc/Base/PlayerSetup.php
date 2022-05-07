bac37b404eadf79bc865f407ea7779dde64a18fe
George Patsoulis <patsoulis.george@gmail.com>
2022-05-07 16:42:41 +0300

    added error log to PlayerSetup class

--- app/inc/Base/PlayerSetup.php
+++ app/inc/Base/PlayerSetup.php
@@ -14,16 +14,19 @@ class PlayerSetup {
     public static function setup_custom_user_meta( int $user_id  ){
         
         if(metadata_exists( 'user', $user_id, 'score_points' )){
+            error('\"score_points\" metadata exists for user with id: ' . $user_id);
             throw new Exception('\"score_points\" metadata exists for user with id: ' . $user_id);
         }
     
         $id = update_user_meta( $user_id, 'score_points', '', true );
     
         if( true === $id){
+            error('\"score_points\" metadata exists for user with id: ' . $user_id);
             throw new Exception('\"score_points\" metadata exists for user with id: ' . $user_id);
         }
     
         if( false === $id){
+            error('fail to create \"score_points\" metadata for user with id: ' . $user_id);
             throw new Exception('fail to create \"score_points\" metadata for user with id: ' . $user_id);
         }
     }
