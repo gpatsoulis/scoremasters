ae7309aa2591e35ff1f0c311dc7063d7383c9b8c
George Patsoulis <patsoulis.george@gmail.com>
2022-05-07 14:08:06 +0300

    minor fix:popup on submit click disable button

--- app/js/activate-prediction-popup.js
+++ app/js/activate-prediction-popup.js
@@ -512,22 +512,28 @@ function setUpPlayersList(data, popup){
 
     let button = popup.querySelector('button[type="submit"]');
 
-    let current = new Date();
-    //current = current.toUTCString();
-    console.log(current);
+    button.addEventListener('click',disableBtn);
 
-    let matchDateTimestamp = popup.querySelector('input[name="match_date"]');
-    console.log(matchDateTimestamp.value);
+    function disableBtn(event){
 
-    let matchDate = new Date(parseInt(matchDateTimestamp.value * 1000));
-    matchDate.setTime(matchDate.getTime() + matchDate.getTimezoneOffset()*60*1000 );
-    //matchDate.setTime(matchDate.getTime() + 180*60*1000 );
-
-    console.log(matchDate);
-
-    if(current.getTime() >= matchDate.getTime()){
-        console.log('match time');
-        button.disabled = true;
-    }
+        let current = new Date();
+        //current = current.toUTCString();
+        console.log(current);
+    
+        let matchDateTimestamp = popup.querySelector('input[name="match_date"]');
+        console.log(matchDateTimestamp.value);
+    
+        let matchDate = new Date(parseInt(matchDateTimestamp.value * 1000));
+        matchDate.setTime(matchDate.getTime() + matchDate.getTimezoneOffset()*60*1000 );
+        //matchDate.setTime(matchDate.getTime() + 180*60*1000 );
+    
+        console.log(matchDate);
+    
+        if(current.getTime() >= matchDate.getTime()){
+            console.log('match time');
+            button.disabled = true;
+        }
+     }
 
  }
+
