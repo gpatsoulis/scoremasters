ac5b332832df08ebb996d9fc13215cf6df9cc0f6
George Patsoulis <patsoulis.george@gmail.com>
2022-05-14 18:35:04 +0300

    version change popup.js

--- app/js/activate-prediction-popup-v2.js
+++ app/js/activate-prediction-popup-v2.js
@@ -613,38 +613,6 @@ function set_reset_first_to_first_option(select){
 }
 */
 
-/*
- function disable_form_on_date(event, id, instance){
-    let popup = instance["$element"][0];
-
-    let button = popup.querySelector('button[type="submit"]');
-
-    button.addEventListener('click',disableBtn);
-
-    function disableBtn(event){
-
-        let current = new Date();
-        //current = current.toUTCString();
-        console.log(current);
-    
-        let matchDateTimestamp = popup.querySelector('input[name="match_date"]');
-        console.log(matchDateTimestamp.value);
-    
-        let matchDate = new Date(parseInt(matchDateTimestamp.value * 1000));
-        matchDate.setTime(matchDate.getTime() + matchDate.getTimezoneOffset()*60*1000 );
-        //matchDate.setTime(matchDate.getTime() + 180*60*1000 );
-    
-        console.log(matchDate);
-    
-        if(current.getTime() >= matchDate.getTime()){
-            console.log('match time');
-            button.disabled = true;
-        }
-     }
-
- }
- */
-
 function possible_player_points(event, id, instance) {
     let popup = instance["$element"][0];
 
