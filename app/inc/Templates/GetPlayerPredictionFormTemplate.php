86e6cd4f21e25519e2312838b74018ad68fb992e
George Patsoulis <patsoulis.george@gmail.com>
2022-11-17 21:08:45 +0200

    select2 added

--- app/inc/Templates/GetPlayerPredictionFormTemplate.php
+++ app/inc/Templates/GetPlayerPredictionFormTemplate.php
@@ -30,10 +30,10 @@ final class GetPlayerPredictionFormTemplate implements TemplateInterface
 		$str2 = '$(document).ready(function() {$(\'.select2-player\').select2();});';
 
         $template_html = <<<HTML
-		<!--<script
+		<script
 			  src="https://code.jquery.com/jquery-3.6.1.slim.min.js"
 			  integrity="sha256-w8CvhFs7iHNVUtnSP0YKEg00p9Ih13rlL9zGqvLdePA="
-			  crossorigin="anonymous"></script>-->
+			  crossorigin="anonymous"></script>
 		<link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
 		<script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
 
@@ -55,11 +55,8 @@ final class GetPlayerPredictionFormTemplate implements TemplateInterface
 			</fieldset>
         </form>
 		<script>
-			window.onload = function () {
-				{$str1}
-				{$str2}
-			}
-			
+			{$str1}
+			{$str2}
 		</script>
         {$prediction_output}
 HTML;
