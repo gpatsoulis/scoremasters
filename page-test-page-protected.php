44e365e6a6614a1cc7987d1291bfaceb2a2d2f65
George Patsoulis <patsoulis.george@gmail.com>
2022-11-22 17:13:29 +0200

    calculate weekly points

--- page-test-page-protected.php
+++ page-test-page-protected.php
@@ -5,6 +5,8 @@ use Scoremasters\Inc\Classes\WeeklyMatchUps;
 use Scoremasters\Inc\Services\CalculateWeeklyMatchups;
 
 
+require_once dirname(__FILE__) . '/app/inc/tools/calculate_weekly_score_for_fixture.php';
+scm_match_trigger_players_weekly_point_calculation(7349);
 //var_dump($test->get_all_matchups());
 
 echo 'hello';
