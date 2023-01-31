32de6b88c90085b9bb68e1b74363764dc94123cc
George Patsoulis <patsoulis.george@gmail.com>
2023-01-31 22:17:16 +0200

    minor update js

--- app/js/activate-prediction-popup-v2.2.js
+++ app/js/activate-prediction-popup-v2.2.js
@@ -576,7 +576,7 @@ function in_option_render_points(){
             return;
         }
 
-        option.innerText += '     πόντοι: ' + points;
+        option.innerText += '&nbsp;&nbsp;|&nbsp;&nbsp;πόντοι: ' + points;
     }
 
 }
