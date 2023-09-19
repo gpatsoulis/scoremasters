12bfab6775c27373abcdc6a9d115f91a6d7a08e7
George Patsoulis <patsoulis.george@gmail.com>
2023-09-19 22:34:43 +0300

    debug

--- page-17812.php
+++ page-17812.php
@@ -1,13 +1,20 @@
-<?php 
+<?php
 
 use Scoremasters\Inc\Base\ScmData;
-use Scoremasters\Inc\Services\CalculateLeaguesCupPoints;
 use Scoremasters\Inc\Classes\WeeklyLeagueMatchUps;
+use Scoremasters\Inc\Services\CalculateLeaguesCupPoints;
 
-if (!current_user_can( 'manage_options' )) exit;
+if (!current_user_can('manage_options')) {
+    exit;
+}
 
-$matchups = new WeeklyLeagueMatchUps(17298);
-$matchups = new WeeklyLeagueMatchUps(17264);
+$weekly_competition = ScmData::get_current_scm_competition_of_type('leagues-cup');
+if ($weekly_competition->ID < 0) {
+    error_log(__METHOD__ . ' ----ERROR EVENT---- error calculating add_leagues_cup_matchups');
+    return;
+}
+
+$matchups = new WeeklyLeagueMatchUps($weekly_competition->ID);
 $matchups->get_all_matchups();
 $currentMatchups = $matchups->for_fixture_id(17298);
 
