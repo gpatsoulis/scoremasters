120c427fc8f45ac0f5390c714590fa53c14466d7
George Patsoulis <patsoulis.george@gmail.com>
2023-09-19 21:18:36 +0300

    temp script for leagues cup points recalculation

---
+++ page-recalculate-leagues-cup.php
@@ -0,0 +1,15 @@
+<?php 
+
+use Scoremasters\Inc\Base\ScmData;
+use Scoremasters\Inc\Services\CalculateLeaguesCupPoints;
+use Scoremasters\Inc\Classes\WeeklyLeagueMatchUps;
+
+if (!current_user_can( 'manage_options' )) exit;
+
+$matchups = new WeeklyLeagueMatchUps(17298);
+$matchups->get_all_matchups();
+$currentMatchups = $matchups->for_fixture_id(17298);
+
+$score = new CalculateLeaguesCupPoints($currentMatchups, 17298);
+$score->calculate();
+$score->save();
