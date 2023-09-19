a82a74173622f49c16075fe5f5a8974e0b221fd9
George Patsoulis <patsoulis.george@gmail.com>
2023-09-19 22:39:49 +0300

    debug

--- page-17812.php
+++ page-17812.php
@@ -8,6 +8,8 @@ if (!current_user_can('manage_options')) {
     exit;
 }
 
+echo '-- recalcution started!!!';
+
 $weekly_competition = ScmData::get_current_scm_competition_of_type('leagues-cup');
 if ($weekly_competition->ID < 0) {
     error_log(__METHOD__ . ' ----ERROR EVENT---- error calculating add_leagues_cup_matchups');
@@ -15,7 +17,10 @@ if ($weekly_competition->ID < 0) {
 }
 
 $matchups = new WeeklyLeagueMatchUps($weekly_competition->ID);
-$matchups->get_all_matchups();
+$all = $matchups->get_all_matchups();
+
+var_dump($all);
+
 $currentMatchups = $matchups->for_fixture_id(17298);
 
 $score = new CalculateLeaguesCupPoints($currentMatchups, 17298);
